aaeb913653f0b95c8e080cbe53b8f3170b68e65d
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-06 18:30:38 +0100

    configurar favoritos y localstorage

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,51 @@
-const toggleFavorite=()=>{
-    let favorites=JSON.parse(localStorage.getItem("favorites")) || []
+import { useEffect, useState } from "react"
+import { getCoins } from "../services/api"
+import { Link } from "react-router-dom"
 
-    if(favorites.includes(confirm.id)){
-        favorites=favorites.filter(f=f!==confirm.id)
-    }else{
-        favorites.push(confirm.id)
+export default function Favorites() {
+
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || []
+
+    const fetchCoins = async () => {
+
+      const data = await getCoins()
+
+      const filteredCoins = data.filter((coin) =>
+        favorites.includes(coin.id)
+      )
+
+      setCoins(filteredCoins)
     }
-    localStorage.setItem("favorites",JSON.stringify(favorites))
+
+    fetchCoins()
+
+  }, [])
+
+  if (coins.length === 0) {
+    return <p>No favorite cryptocurrencies</p>
+  }
+
+  return (
+    <div>
+
+      <h1>Favorites</h1>
+
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+
+            <Link to={`/coin/${coin.id}`}>
+              {coin.name}
+            </Link>
+
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,17 @@
-const API_URL="https://rest.coincap.io/v3"
-const API_KEY=import.meta.env.VITE_API_KEY
+const API_URL = "https://rest.coincap.io/v3"
 
-export const getCoins=async()=>{
-    const res=await fetch(`${API_URL}/assets?apiKey=${API_KEY}`)
-    const data=await res.json()
-    return data.data
+export const getCoins = async () => {
+
+  const res = await fetch(`${API_URL}/assets`)
+  const data = await res.json()
+
+  return data.data
 }
 
-export const getCoin=async(id)=>{
-    const res=await fetch(`${API_URL}/assets/${id}?apiKey=${API_KEY}`)
-    const data= await res.json()
-    return data.data
+export const getCoin = async (id) => {
+
+  const res = await fetch(`${API_URL}/assets/${id}`)
+  const data = await res.json()
+
+  return data.data
 }
